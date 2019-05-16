3e535cc4b29fa535aae41ee70f6f95626a36415e
ro-mak <eee127@yandex.ru>
2019-05-16 20:00:29 +0300

    -corrected mistakes

--- app/src/main/java/ru/makproductions/tinkoffinternship/ui/activity/MainActivity.kt
+++ app/src/main/java/ru/makproductions/tinkoffinternship/ui/activity/MainActivity.kt
@@ -1,21 +1,20 @@
 package ru.makproductions.tinkoffinternship.ui.activity
 
 import android.os.Bundle
-import android.support.v7.app.AppCompatActivity
+import com.arellomobile.mvp.MvpActivity
 import com.arellomobile.mvp.presenter.InjectPresenter
 import com.arellomobile.mvp.presenter.ProvidePresenter
 import io.reactivex.android.schedulers.AndroidSchedulers
-import kotlinx.android.synthetic.main.news_list_fragment.*
 import ru.makproductions.tinkoffinternship.App
 import ru.makproductions.tinkoffinternship.R
 import ru.makproductions.tinkoffinternship.presenter.MainPresenter
 import ru.makproductions.tinkoffinternship.ui.adapter.NewsAdapter
 import ru.makproductions.tinkoffinternship.view.main.MainView
 
-class MainActivity : AppCompatActivity(), MainView {
+class MainActivity : MvpActivity(), MainView {
 
     @InjectPresenter
-    private lateinit var mainPresenter: MainPresenter
+    lateinit var mainPresenter: MainPresenter
 
     @ProvidePresenter
     fun providePresenter(): MainPresenter {
@@ -29,7 +28,7 @@ class MainActivity : AppCompatActivity(), MainView {
         setContentView(R.layout.activity_main)
         App.instance.appComponent.inject(this)
         val newsAdapter = NewsAdapter(mainPresenter.getNewsListPresenter())
-        news_recycler.adapter = newsAdapter
+        //news_recycler.adapter = newsAdapter
     }
 
 }
